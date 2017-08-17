265ab1d3567c499c78ae83176407b49750a17ed1
Gernot <Gernot@404-Not-Found.local>
2017-08-17 19:57:32 +0200

    tweaking

--- js/game.js
+++ js/game.js
@@ -1,12 +1,7 @@
 
-//spieler array dummy
-var spielerArray = new Array (2);
+//spieler array
+var spielerArray = [[],[]]
 
-for (var i = 0; i < spielerArray.length; i++) {
-  spielerArray[i] = new Array (2)
-  spielerArray [0] [0] =[]
-  spielerArray [0] [1] =[]
-}
 
 
 //player points
@@ -23,14 +18,13 @@ var coordinates = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
 
 //felderauswahl
 var field = document.querySelectorAll('.box');
-for (var i = 0; i < field.length; i++) {
-  console.log(field[i]);
-}
 
 var markPosition = document.getElementsByClassName('inside')[0];
 var separate = Array.prototype.slice.call(field, 0);
-//start button
+//start & reset button
 var start = document.getElementsByClassName('btn')[0];
+var reset = document.getElementsByClassName('reset')[0];
+
 
 //pointField
 var PointFieldA = document.getElementsByClassName('punkte_Sp1')[0]
@@ -41,8 +35,9 @@ PointFieldB.innerHTML = pointCounterB;
 
 
 var inputPlayer2 = document.querySelector('.player_input');
-
 var inputComputer = document.querySelector('.computer_input');
+
+
 
 
 
@@ -56,18 +51,20 @@ function classNaming () {
 
 
 function getPoints(box,i,turn) {
+
   if (box == "box") {
 
     var selection = coordinates[i];
 
     function playerSelection() {
 
-      spielerArray [0] [turn].push(selection)
-      console.log(spielerArray [0] [0] + "  array 0");
-      console.log(spielerArray [0] [1] + "  array 1");
+
+      spielerArray [turn].push(selection)
+      console.log(spielerArray [0] + "  Spieler 1");
+      console.log(spielerArray [1] + "  Spieler 2");
     }
 
-    playerSelection(turn,spielerArray [0] [0], spielerArray [0] [1]);
+    playerSelection();
     markFields(turn,i,playerMarkA)
     field[i].className = "box_dummy";
 
@@ -82,7 +79,6 @@ function getPoints(box,i,turn) {
 
 function markFields(turn,i) {
   if (turn == 0) {
-    console.log(field[i].childNodes);
     field[i].childNodes[1].innerHTML = playerMarkA
   }
 
@@ -92,64 +88,127 @@ function markFields(turn,i) {
 }
 
 
-function goal(pointCounterA,pointCounterB) {
-  for (var i = 0; i < field.length; i++) {
-    console.log(field[i].className);
-    var test = field[i].className
-    if (field[i].className == field.className) {
-      alert("foo")
+function computerChoose() {
+  console.log("insied computerChoose");
+  var loop = true;
+
+  while (loop == true) {
+    var computerNumber = Math.floor((Math.random() * 8) + 1)
+    computerSelector = coordinates[computerNumber]
+    console.log(computerSelector);
+
+    var checkPlayerArray = spielerArray[0].indexOf(computerSelector)
+    console.log(checkPlayerArray);
+
+    var checkOwnArray = spielerArray[1].indexOf(computerSelector)
+    console.log(checkOwnArray + "  own array");
+
+    if (checkPlayerArray > -1 || checkOwnArray > -1) {
+      console.log("again");
+      loop = true;
     }
 
-
+    if (checkPlayerArray === -1 && checkOwnArray === -1) {
+      loop = false;
+      console.log(loop);
+      var collection = [computerNumber,computerSelector];
+      return collection;
+    }
+    console.log(loop + " outer loop");
   }
 
-  if (pointCounterA > 1) {
-    alert("Player 1 Win!")
-  }
-
-  if (PointFieldB > 1) {
-    alert("Player 2 Win!")
-  }
 }
 
-computer
-
-function computer(turn) {
-  if (turn == 1 && inputComputer.checked == true) {
-    alert("buhuhu")
-  }
+function computerPush(hander) {
+  console.log(hander + " insider push");
+  spielerArray[1].push(hander[1])
+  console.log(spielerArray[1]);
+  field[hander[0]].childNodes[1].innerHTML = playerMarkB;
 }
 
-computer()
+
 
 //setup
 function setup() {
 
   var turn = 0;
-
   classNaming();
-  computer(turn)
   separate.forEach(function(field, i) {
     field.addEventListener("click", function() {
       var box = separate[i].className;
-      console.log(i);
-      getPoints(box,i, turn, playerMarkA, playerMarkB);
-      points(turn,box);
-      goal(pointCounterA,pointCounterB);
+      getPoints(box,i, turn);
+      checkWin(turn)
+
+      draw()
+
       if (box == "box") {
         turn = 1 - turn;
       }
-      console.log(turn + " outer");
+
+      setTimeout(function() {
+
+        if (turn == 1 && inputComputer.checked == true) {
+          console.log("my turn");
+          //choose nummber and backcheck if nummber allready choosen
+
+          var hander = computerChoose()
+          //push number in array
+          computerPush(hander)
+          //check winning conditions
+          checkWin(turn)
+          draw()
+
+          turn = 1 - turn;
+        }
+      },500)
+
     })
   })
+}
+
+
+function draw() {
+  var full = true;
+  for (var i = 0; i < field.length; i++) {
+    if (field[i].className == "box") {
+      full = false;
+    }
+  }
+
+  if (full == true) {
+    alert("Its a draw")
+    cleanUp()
+  }
+
+}
+
+
+function cleanUp(turn) {
+
+  spielerArray [0].length = 0;
+  spielerArray [1].length = 0;
+  inputComputer.checked = false;
+
+  for (var i = 0; i < field.length; i++) {
+    field[i].className = "box"
+    field[i].childNodes[1].innerHTML = ""
+  }
+  turn = 0
   console.log(turn);
+  console.log("ende");
 }
 
 
 
 start.addEventListener("click", function() {
+
   alert("Spielstart")
   setup();
+})
+
+reset.addEventListener("click", function() {
+  alert("reset")
+  cleanUp()
 })
 
 
@@ -157,111 +216,78 @@ start.addEventListener("click", function() {
 
 
 
+//pointsystem
+var winArray =
+  [["A1", "A2", "A3"],
+  ["B1", "B2", "B3"],
+  ["C1", "C2", "C3"],
+  ["A1", "B1", "C1"],
+  ["A2", "B2", "C2"],
+  ["A3", "B3", "C3"],
+  ["A1", "B2", "C3"],
+  ["C1", "B2", "A3"],]
 
 
 
-//define pointsystem
-var horizontal = new Array (3)
-
-for (var i = 0; i < 3; i++) {
-  horizontal[i] = new Array (3)
-  horizontal [0] [0] = ["A1", "A2", "A3"];
-  horizontal [0] [1] = ["B1", "B2", "B3"];
-  horizontal [0] [2] = ["C1", "C2", "C3"];
-}
-
-
-var vertikal = new Array (3)
-
-for (var i = 0; i < 3; i++) {
-  vertikal[i] = new Array (3)
-  vertikal [0] [0] = ["A1", "B1", "C1"];
-  vertikal [0] [1] = ["A2", "B2", "C2"];
-  vertikal [0] [2] = ["A3", "B3", "C3"];
-}
-
-
-var diagonal = new Array (2)
-
-for (var i = 0; i < 2; i++) {
-  diagonal[i] = new Array (2);
-  diagonal [0] [0] = ["A1", "B2", "C3"]
-  diagonal [0] [1] = ["C1", "B2", "A3"]
-}
 
 function pointCount(turn) {
-  console.log("inside");
+
   if (turn == 0) {
-    alert("Player 1. You made a point")
+    alert("Player 1. You win")
     pointCounterA += 1;
     PointFieldA.innerHTML = pointCounterA
+
+    cleanUp()
 
 
 
   }
 
   if (turn == 1) {
-    alert("Player 2. You made a point")
+    alert("Player 2. You win")
     pointCounterB += 1;
     PointFieldB.innerHTML = pointCounterB
   }
+
+  cleanUp()
 }
 
-//system zum vergleich mit spieler array
-function points(turn, box) {
-
-  //horizontal
-  if ($(spielerArray [0] [turn]).not(horizontal [0] [0]).length == 0 && $(horizontal [0] [0]).not(spielerArray [0] [turn]).length === 0) {
-      console.log("point!");
-      pointCount(turn,pointCounterA,pointCounterB);
 
 
+
+function winingArray(eachArray,counter,win,turn) {
+  for (var i = 0; i < spielerArray [turn].length; i++) {
+
+    var checkArray = eachArray.indexOf(spielerArray [turn] [i])
+
+    if (checkArray > -1) {
+      counter ++
+    }
+  }
+
+  if (counter === 3) {
+    win = true;
 
   }
 
-  if ($(spielerArray [0] [turn]).not(horizontal [0] [1]).length === 0 && $(horizontal [0] [1]).not(spielerArray [0] [turn]).length === 0) {
-    console.log("point!");
-    pointCount(turn,pointCounterA,pointCounterB);
 
-
+  if (win === true) {
+    pointCount(turn)
   }
-
-  if ($(spielerArray [0] [turn]).not(horizontal [0] [0]).length === 0 && $(horizontal [0] [2]).not(spielerArray [0] [turn]).length === 0) {
-    console.log("point!");
-    pointCount(turn,pointCounterA,pointCounterB);
-
-  }
-
-  //vertikal
-  if ($(spielerArray [0] [turn]).not(vertikal [0] [0]).length === 0 && $(vertikal [0] [0]).not(spielerArray [0] [turn]).length === 0) {
-    console.log("point!");
-    pointCount(turn,pointCounterA,pointCounterB);
+}
 
 
-  }
 
-  if ($(spielerArray [0] [turn]).not(vertikal [0] [1]).length === 0 && $(vertikal [0] [1]).not(spielerArray [0] [turn]).length === 0) {
-    console.log("point!");
-    pointCount(turn,pointCounterA,pointCounterB);
+function checkWin(turn) {
+  var index;
+  var win = false;
+  var counter = 0;
 
-  }
+  if (spielerArray [turn].length === 3 || spielerArray [turn].length > 3) {
+    for (var i = 0; i < winArray.length; i++) {
+      var eachArray = winArray[i]
 
-  if ($(spielerArray [0] [turn]).not(vertikal [0] [2]).length === 0 && $(vertikal [0] [2]).not(spielerArray [0] [turn]).length === 0) {
-    console.log("point!");
-    pointCount(turn,pointCounterA,pointCounterB);
-
-  }
-
-  //diagonal
-  if ($(spielerArray [0] [turn]).not(diagonal [0] [0]).length === 0 && $(diagonal [0] [0]).not(spielerArray [0] [turn]).length === 0) {
-    console.log("point!");
-    pointCount(turn,pointCounterA,pointCounterB);
-
-  }
-
-  if ($(spielerArray [0] [turn]).not(diagonal [0] [1]).length === 0 && $(diagonal [0] [1]).not(spielerArray [0] [turn]).length === 0) {
-    console.log("point!");
-    pointCount(turn,pointCounterA,pointCounterB);
-
+      winingArray(eachArray,counter,win,turn)
+    }
   }
 }
